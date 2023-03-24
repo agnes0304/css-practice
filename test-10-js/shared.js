703c504b5128e141b2788580eb94e97518e04ac2
agnes0304 <jiwoochoi0304@gmail.com>
2023-03-24 16:25:53 +0900

    working with js-classList

--- test-10-js/shared.js
+++ test-10-js/shared.js
@@ -1,12 +1,39 @@
 const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
+const modalNoBtn = document.querySelector('.modal__action--negative');
 const selectPlanBtns = document.querySelectorAll('.plan button');
+const toggleBtn = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav');
+
 // var 대신에 const 사용
 // console.dir(backdrop)
-
 for (let i = 0; i < selectPlanBtns.length; i++) {
     selectPlanBtns[i].addEventListener('click', function(){
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        // modal.className = 'open';
+        modal.classList.add('open');
+        backdrop.classList.add('open');
     });
 }
+
+function closeModal(){
+    // backdrop.style.display='none';
+    // modal.style.display='none';
+    modal.classList.remove('open');
+    backdrop.classList.remove('open');
+}
+
+backdrop.addEventListener('click', function(){
+    // mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open');
+    closeModal();
+});
+
+modalNoBtn.addEventListener('click', closeModal);
+
+toggleBtn.addEventListener('click', function(){
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+});
+
